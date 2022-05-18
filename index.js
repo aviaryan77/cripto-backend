99c644e49f53654f1546a80569069ef1139b8bd7
aviaryan77 <aviaryan77@gmail.com>
2022-05-18 15:37:56 +0530

    first comit

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT;
+
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const socketHandler = socketIO(server);
+
+socketHandler.on('connection', (socket) => {
+  socket.on('connect_error', (err) => {
+    console.log(`connection error: ${err}`);
+  });
+  socket.on('disconnect', () => {
+    console.log(`Client disconnected`);
+  });
+
+  console.log(`Client connected`);
+});
+
+const getPrices = () => {
+  axios
+    .get(process.env.LIST_URL, {
+      headers: { 'x-messari-api-key': process.env.CRYPTO_API_KEY },
+    })
+    .then((res) => {
+      const priceList = res.data.data.map((item) => ({
+        id: item.id,
+        name: item.symbol,
+        price: item.metrics.market_data.price_usd,
+      }));
+      socketHandler.emit('crypto', priceList);
+    })
+    .catch((error) => {
+      console.log(error);
+      socketHandler.emit('crypto', {
+        error: true,
+        message: ' Error fetching prices from API',
+      });
+    });
+};
+
+setInterval(() => {
+  getPrices();
+}, 6000000);
+
+app.get('./cryptos/profile/:id', (req, res) => {
+  const cryptoId = req.params.id;
+  if (!cryptoId) {
+    res.json({ error: true, message: 'Missing crypto id in the url' });
+  }
+
+  axios
+    .get(`https://data.messari.io/api/v2/assets/${cryptoId}/profile`, {
+      headers: { 'x-messari-api-key': process.env.CRYPTO_API_KEY },
+    })
+    .then((responseData) => {
+      res.json(responseData.data.data);
+    })
+    .catch((err) => {
+      res.json('crypto', {
+        error: true,
+        message: 'Error fetching price data from api',
+        errorDetails: err,
+      });
+    });
+});
+
+app.get('./cryptos/market-data/:id', (req, res) => {
+  const cryptoId = req.params.id;
+  if (!cryptoId) {
+    res.json({ error: true, message: 'Missing crypto id in the url' });
+  }
+
+  axios
+    .get(
+      `https://data.messari.io/api/v1/assets/${assetKey}/metrics/market-data`,
+      {
+        headers: { 'x-messari-api-key': process.env.CRYPTO_API_KEY },
+      }
+    )
+    .then((responseData) => {
+      res.json(responseData.data.data);
+    })
+    .catch((err) => {
+      res.json('crypto', {
+        error: true,
+        message: 'Error fetching price data from api',
+        errorDetails: err,
+      });
+    });
+});
